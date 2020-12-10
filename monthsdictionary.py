d792cc69220b8352c64fb617f7ec47ea20d4a05a
Vatsal <73216235+Desantonio@users.noreply.github.com>
2020-12-10 12:41:16 +0530

    Add files via upload
    monthscommit

---
+++ monthsdictionary.py
@@ -0,0 +1,23 @@
+# A Months dictionary
+
+Months = {
+    "Jan" : "January",
+    "Feb" : "February",
+    "Mar" : "March",
+    "Apr" : "April",
+    "May" : "May",
+    "Jun" : "June",
+    "Jul" : "July",
+    "Aug" : "August",
+    "Sep" : "September",
+    "Oct" : "October",
+    "Nov" : "November",
+    "Dec" : "December",
+}
+
+print(Months["Jan"])
+print(Months.get("Mar"))
+
+print(Months["Dec"])
+print(Months.get("Jul"))
+
